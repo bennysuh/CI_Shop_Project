4c1bd883aee4795bce2c45574f2947c084719e2d
DarkBlue <2580288008@qq.com>
2016-12-10 18:27:30 +0800

    Add some functions to the goods module

--- application/controllers/admin/Goods.php
+++ application/controllers/admin/Goods.php
@@ -14,11 +14,25 @@ class Goods extends Admin_Controller{
 		$this->load->model('Brand_model');
 	}
 	//显示商品列表
-	public function index(){
+	public function index($page = ""){
 		$data['cates'] = $this->Category_model->list_cate();
 		$data['brands'] = $this->Brand_model->list_brand();
 		$data['suppliers'] = $this->Goods_model->get_all_sup();
-		$data['goods'] = $this->Goods_model->get_all_goods();
+		//加载并配置分页类
+		$this->load->library('pagination');
+		$config['base_url'] = site_url('admin/Goods/index');
+		$config['uri_segment'] = 4;
+		$config['total_rows'] = $this->Goods_model->get_all_nums();
+		$config['per_page'] = 3;
+		$config['first_link'] = '首页';
+		$config['last_link'] = '尾页';
+		$config['prev_link'] = '上一页';
+		$config['next_link'] = '下一页';
+		$this->pagination->initialize($config);
+		$limit = $config['per_page'];
+		//查询分页数据
+		$data['links'] = $this->pagination->create_links();
+		$data['goods'] = $this->Goods_model->get_by_page($page,$limit);
 		$this->load->view('goods_list.html',$data);
 	}
 	//显示添加表单
@@ -132,7 +146,154 @@ class Goods extends Admin_Controller{
 		$data['cates'] = $this->Category_model->list_cate();
 		$data['brands'] = $this->Brand_model->list_brand();
 		$data['suppliers'] = $this->Goods_model->get_all_sup();
+		$data['galarys'] = $this->Goods_model->get_goods_galary($goods_id);
 		$this->load->view('goods_edit.html',$data);
+	}
+	//更新商品信息
+	public function update(){
+		//封装提交的数据
+		$goods_id = $this->input->post('goods_id');
+		$data['goods_name'] = $this->input->post('goods_name');
+		$data['cat_id'] = $this->input->post('cat_id');
+		$data['brand_id'] = $this->input->post('brand_id');
+		$data['suppliers_id'] = $this->input->post('suppliers_id');
+		$data['market_price'] = $this->input->post('market_price');
+		$data['user_price'] = $this->input->post('user_price');
+		$data['shop_price'] = $this->input->post('shop_price');
+		$data['promote_price'] = $this->input->post('promote_price');
+		$data['promote_start_time'] = strtotime($this->input->post('promote_start_time'));
+		$data['promote_end_time'] = strtotime($this->input->post('promote_end_time'));
+		$data['goods_weight'] = $this->input->post('goods_weight')*$this->input->post('weight_unit');
+		$data['goods_number'] = $this->input->post('goods_number');
+		$data['warn_number'] = $this->input->post('warn_number');
+		$data['goods_desc'] = $this->input->post('goods_desc');
+		$data['goods_brief'] = $this->input->post('goods_brief');
+		$data['goods_type'] = $this->input->post('goods_type');
+		$data['keywords'] = $this->input->post('keywords');
+		$data['is_promote'] = isset($_POST['is_promote']) ? $this->input->post('is_promote') : 0;
+		$data['is_new'] = isset($_POST['is_new']) ? $this->input->post('is_new') : 0;
+		$data['is_hot'] = isset($_POST['is_hot']) ? $this->input->post('is_hot') : 0;
+		$data['is_best'] = isset($_POST['is_best']) ? $this->input->post('is_best') : 0;
+		$data['is_onsale'] = isset($_POST['is_onsale']) ? $this->input->post('is_onsale') : 0;
+		$data['is_alone_sale'] = isset($_POST['is_alone_sale']) ? $this->input->post('is_alone_sale') : 0;
+		$data['is_shipping'] = isset($_POST['is_alone_shipping']) ? $this->input->post('is_shipping') : 0;
+		$data['add_time'] = time();
+		//判断是否上传了文件
+		if($_FILES['goods_img']['error'] != 4){
+			//配置文件上传类
+			$config['upload_path'] = './public/uploads/';
+			$config['allowed_types'] = 'jpg|gif|png';
+			$config['max_size'] = 10000;
+			$this->load->library('upload',$config);
+			//文件上传成功
+			if($this->upload->do_upload('goods_img')){
+				$res = $this->upload->data();
+				$data['goods_img'] = $res['file_name'];
+				//配置图片处理类
+				$config_img['source_image'] =  "./public/uploads/".$res['file_name'];
+				$config_img['create_thumb'] = true;
+				$config_img['maintain_ratio'] = true;
+				$config_img['width'] = 160;
+				$config_img['height'] = 160;
+				$this->load->library('image_lib', $config_img);
+				//图片处理是否成功
+				if($this->image_lib->resize()){
+					$data['goods_thumb'] = $res['raw_name'].$this->image_lib->thumb_marker.$res['file_ext'];
+					if($goods_id = $this->Goods_model->update_goods($data)){
+						$attr_ids = $this->input->post('attr_id_list');
+						$attr_values = $this->input->post('attr_value_list');
+						//更新商品属性信息
+						foreach($attr_values as $key => $value){
+							if(!empty($value)){
+								$data2['attr_id'] = $attr_ids[$key];
+								$data2['attr_value'] = $value;
+								$condition = array('goods_id'=>$goods_id);
+								$this->db->where($condition);
+								$this->db->update('goods_attr',$data2);
+							}
+						}
+						//获取商品相册信息
+						$img_descs = $this->input->post('img_desc');
+						$info = $this->upload->multiple('img_url');
+						//多文件上传并插入数据库
+						if(empty($info['error'])){
+							for($i=0;$i<count($info['files']);$i++){
+								$data_img['img_url'] = $info['files'][$i]['file_name'];
+								$data_img['thumb_url'] = "space";
+								$data_img['img_desc'] = $img_descs[$i];
+								$this->Goods_model->update_to_galary($goods_id,$data_img);
+							}
+					  }
+						$data['message'] = "更新商品成功!";
+						$data['wait'] = 3;
+						$data['url'] = site_url('admin/Goods/index');
+						$this->load->view('message.html',$data);
+					}else{
+						$data['message'] = "更新商品失败!";
+						$data['wait'] = 3;
+						$data['url'] = site_url('admin/Goods/edit').'/'.$goods_id;
+						$this->load->view('message.html',$data);
+					}
+			}else{
+				$data['message'] = $this->image_lib->display_errors();
+				$data['wait'] = 3;
+				$data['url'] = site_url('admin/Goods/edit').'/'.$goods_id;
+				$this->load->view('message.html',$data);
+			}
+		}else{
+			$data['message'] = $this->upload->display_errors();
+			$data['wait'] = 3;
+			$data['url'] = site_url('admin/Goods/edit').'/'.$goods_id;
+			$this->load->view('message.html',$data);
+		}
+	}
+}
+	//搜索商品信息
+	public function search($page = ""){
+		//接受搜索条件
+		if($this->input->post('submit') !== null){
+			$cat_id = $this->input->post('cat_id')==-1 ? "*" : $this->input->post('cat_id');
+			$brand_id = $this->input->post('brand_id')==-1 ? "*" : $this->input->post('brand_id');
+			$intro_type = $this->input->post('intro_type') ;
+			$suppliers_id = $this->input->post('suppliers_id')==-1 ? "*" : $this->input->post('suppliers_id');
+			$is_on_sale = $this->input->post('is_on_sale')==-1 ? "*" : $this->input->post('is_on_sale');
+			$keywords = $this->input->post('keywords')=="" ? "*" : $this->input->post('keywords');
+			//封装搜索条件
+			$condition = array();
+			if($intro_type != "-1" && $intro_type != "all_type"){
+				$condition[$intro_type] = 1;
+			}else if($intro_type == "all_type"){
+				$condition['is_best'] = $condition['is_new'] = $condition['is_hot'] = $condition['is_promote'] = 1;
+			}
+			$cat_id == "*" || $condition['cat_id'] = intval($cat_id);
+			$brand_id == "*" || $condition['brand_id'] = intval($brand_id);
+			$suppliers_id == "*" || $condition['suppliers_id'] = intval($suppliers_id);
+			$is_on_sale == "*" || $condition['is_onsale'] = intval($is_on_sale);
+			$keywords == "*" || $condition['keywords'] = $keywords;
+			//将搜索条件放入session中以便分页
+			$this->session->set_userdata('pageinfo',$condition);
+		}
+		$data['cates'] = $this->Category_model->list_cate();
+		$data['brands'] = $this->Brand_model->list_brand();
+		$data['suppliers'] = $this->Goods_model->get_all_sup();
+		//加载并配置分页类
+		$this->load->library('pagination');
+		$config['base_url'] = site_url('admin/Goods/search');
+		$config['uri_segment'] = 4;
+		$config['total_rows'] = $this->Goods_model->get_all_search($this->session->userdata('pageinfo'));
+		$config['per_page'] = 3;
+		$config['first_link'] = '首页';
+		$config['last_link'] = '尾页';
+		$config['prev_link'] = '上一页';
+		$config['next_link'] = '下一页';
+		$this->pagination->initialize($config);
+		$limit = $config['per_page'];
+		//查询分页数据
+		$data['links'] = $this->pagination->create_links();
+		$data['goods'] = $this->Goods_model->get_by_search($this->session->userdata('pageinfo'),$page,$limit);
+		//显示搜索结果
+		$this->load->view('goods_list.html',$data);
+
 	}
 
 	//处理ajax获取分类属性数据的请求
--- application/models/Goods_model.php
+++ application/models/Goods_model.php
@@ -13,17 +13,42 @@ class Goods_model extends CI_Model{
 		$query = $this->db->insert($this->table,$data);
 		return $query ? $this->db->insert_id() : false;
 	}
-	//获取所有的商品
-	public function get_all_goods(){
+	//获取所有的商品数量
+	public function get_all_nums(){
 		$query = $this->db->get($this->table);
+		return $query->num_rows();
+	}
+	//根据分页数据获取商品
+	public function get_by_page($offset,$limit){
+		$query = $this->db->limit($limit,$offset)->get($this->table);
 		return $query->result_array();
 	}
+	//获取搜索商品总数
+	public function get_all_search($condition){
+		$query = $this->db->where($condition)->get($this->table);
+		return $query->num_rows();
+	}
+
+	//根据条件搜索商品
+	public function get_by_search($condition,$offset,$limit){
+		$query = $this->db->where($condition)->limit($limit,$offset)->get($this->table);
+		return $query->result_array();
+	}
+
 	//根据编号获取商品
 	public function get_goods($goods_id){
 		$condition['goods_id'] = $goods_id;
 		$query = $this->db->where($condition)->get($this->table);
 		return $query->row_array();
 	}
+	//更新商品信息
+	public function update_goods($goods_id,$info){
+		$condition = array('goods_id'=>$goods_id);
+		$this->db->where($condition);
+		$this->db->update($this->table,$info);
+		return $this->db->affected_rows();
+	}
+
   //获取受到好评商品
 	public function best_goods(){
 		$condition['is_best'] = 1;
@@ -36,9 +61,24 @@ class Goods_model extends CI_Model{
 		$query = $this->db->get($table);
 		return $query->result_array();
 	}
-	//插入至商品相册
+	//插入商品相册
 	public function add_to_galary($galary){
 		$table = "galary";
 		return $this->db->insert($table,$galary);
+	}
+	//更新商品相册
+	public function update_to_galary($goods_id,$data_img){
+		$table = "galary";
+		$condition = array('goods_id'=>$goods_id);
+		$this->db->where($condition);
+		$this->db->update($table,$data_img);
+		return $this->db->affected_rows();
+	}
+	//查询商品相册
+	public function get_goods_galary($goods_id){
+		$table = "galary";
+		$condition = array('goods_id'=>$goods_id);
+		$query = $this->db->where($condition)->get($table);
+		return $query->result_array();
 	}
 }
